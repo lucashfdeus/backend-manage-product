8eae6ef6b74df86a6d0943ee71d27b9154aa7c53
lucashfdeus <lucas.hfdeus@gmail.com>
2024-07-30 01:09:46 -0300

    refactor(ManageProduct): Ajuste arquivo init.sql.

--- ManageProduct/script-docker-compose.bat
+++ ManageProduct/script-docker-compose.bat
@@ -1,22 +1,12 @@
 @echo off
 :: Iniciando os contêineres PostgreSQL e pgAdmin
 echo Starting PostgreSQL and pgAdmin containers...
+
+:: Executando o comando docker-compose para iniciar os contêineres
 docker-compose up -d
 
 :: Informando que os contêineres estão iniciando
 echo Containers are starting up...
-
-:: Pausando para garantir que os contêineres tenham tempo para inicializar
-timeout /t 30
-
-:: Executando o script SQL para configurar o banco de dados
-echo Configuring the database...
-
-:: Substitua o <db_user>, <db_password>, <db_host>, e <db_name> pelos valores adequados
-psql -U <db_user> -h <db_host> -d <db_name> -f setup.sql
-
-:: Informando que a configuração foi concluída
-echo Database configuration complete.
 
 :: Pausando para que a janela do prompt de comando não feche imediatamente
 pause
