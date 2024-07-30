19d6dd4685b89488e115c8e5015ad6f9a5e868ca
lucashfdeus <lucas.hfdeus@gmail.com>
2024-07-30 00:56:34 -0300

    feat(ManageProduct) adicionando arquivo setup.sql

--- ManageProduct/script-docker-compose.bat
+++ ManageProduct/script-docker-compose.bat
@@ -1,15 +1,22 @@
-:: Abra o prompt de comando (cmd) e navegue até o diretório onde o arquivo start_containers.bat e o docker-compose.yml
-:: ...\ManageProduct\docker-compose.yml
-
 @echo off
 :: Iniciando os contêineres PostgreSQL e pgAdmin
 echo Starting PostgreSQL and pgAdmin containers...
-
-:: Executando o comando docker-compose para iniciar os contêineres
 docker-compose up -d
 
 :: Informando que os contêineres estão iniciando
 echo Containers are starting up...
+
+:: Pausando para garantir que os contêineres tenham tempo para inicializar
+timeout /t 30
+
+:: Executando o script SQL para configurar o banco de dados
+echo Configuring the database...
+
+:: Substitua o <db_user>, <db_password>, <db_host>, e <db_name> pelos valores adequados
+psql -U <db_user> -h <db_host> -d <db_name> -f setup.sql
+
+:: Informando que a configuração foi concluída
+echo Database configuration complete.
 
 :: Pausando para que a janela do prompt de comando não feche imediatamente
 pause
